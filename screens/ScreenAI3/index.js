f02c53a34eb9521988fd2d69d830b049fd2ab4b6
Saad bin Abid <saad.abid@crowdbotics.com>
2023-08-18 00:59:34 +0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI3/index.js
+++ screens/ScreenAI3/index.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from "react";
+import { Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 
 const RecipeDetailScreen = () => {
   const [isFavorite, setIsFavorite] = useState(false);
   const recipe = {
-    name: 'Delicious Pasta',
-    image: 'https://tinyurl.com/42evm3m3',
-    ingredients: ['Pasta', 'Tomatoes', 'Garlic', 'Olive Oil', 'Basil'],
-    time_consume: '1. Cook pasta according to package instructions. \n2. In a pan, heat olive oil and sauté garlic until fragrant. \n3. Add tomatoes and cook until softened. \n4. Toss cooked pasta with the tomato sauce. \n5. Garnish with basil leaves and serve hot.'
+    name: "Delicious Pasta",
+    image: "https://tinyurl.com/42evm3m3",
+    ingredients: ["Pasta", "Tomatoes", "Garlic", "Olive Oil", "Basil"],
+    time_consume: "1. Cook pasta according to package instructions. \n2. In a pan, heat olive oil and sauté garlic until fragrant. \n3. Add tomatoes and cook until softened. \n4. Toss cooked pasta with the tomato sauce. \n5. Garnish with basil leaves and serve hot."
   };
 
   const handleFavoritePress = () => {
@@ -21,11 +20,15 @@ const RecipeDetailScreen = () => {
     }} style={styles.image} />
       <Text style={styles.name}>{recipe.name}</Text>
       <Text style={styles.sectionTitle}>Ingredients:</Text>
-      {recipe.ingredients.map((ingredient, index) => <Text key={index} style={styles.ingredient}>{ingredient}</Text>)}
+      {recipe.ingredients.map((ingredient, index) => <Text key={index} style={styles.ingredient}>
+          {ingredient}
+        </Text>)}
       <Text style={styles.sectionTitle}>Time Consume:</Text>
       <Text style={styles.instructions}>{recipe.time_consume}</Text>
       <TouchableOpacity style={[styles.favoriteButton, isFavorite ? styles.favoriteButtonActive : null]} onPress={handleFavoritePress}>
-        <Text style={styles.favoriteButtonText}>{isFavorite ? 'Remove from Favorites' : 'Add to Favorites'}</Text>
+        <Text style={styles.favoriteButtonText}>
+          {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+        </Text>
       </TouchableOpacity>
     </SafeAreaView>;
 };
@@ -33,9 +36,9 @@ const RecipeDetailScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
     paddingHorizontal: 20
   },
   image: {
@@ -46,12 +49,12 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10
   },
   sectionTitle: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 10,
     marginBottom: 5
   },
@@ -64,17 +67,17 @@ const styles = StyleSheet.create({
     marginBottom: 20
   },
   favoriteButton: {
-    backgroundColor: '#ccc',
+    backgroundColor: "#ccc",
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5
   },
   favoriteButtonActive: {
-    backgroundColor: '#ff0000'
+    backgroundColor: "#ff0000"
   },
   favoriteButtonText: {
-    color: '#fff',
-    fontWeight: 'bold'
+    color: "#fff",
+    fontWeight: "bold"
   }
 });
 export default RecipeDetailScreen;
