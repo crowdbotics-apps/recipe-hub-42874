dcb69f085b6fea98168cd6c46a86251d6ad2a614
crowdbotics-apps <team@crowdbotics.com>
2023-08-17 12:57:32 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import UserRecipe
+admin.site.register(UserRecipe)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+class UserRecipe(models.Model):
+    'Generated Model'
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="userrecipe_user",)
+    title = models.CharField(max_length=255,)
+    ingredients = models.CharField(max_length=255,)
+    time_consume = models.TimeField()
+    steps = models.TextField()
+    image = models.ImageField(upload_to="userrecipe/image/images/",)
