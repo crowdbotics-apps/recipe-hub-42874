1a632ebec127e418754dbcb3d864a2371bf3da3d
Crowdbotics <engineering@crowdbotics.com>
2023-08-25 17:58:47 +0000

    Update store.

--- store/recipeconnector/recipeconnector_response_get_Listrecipes.slice.js
+++ store/recipeconnector/recipeconnector_response_get_Listrecipes.slice.js
@@ -1,16 +1,15 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { apiService } from "./api";
-export const recipeconnector_get_api_v1_userrecipe_list = createAsyncThunk("recipeconnector_response_get_Listrecipes/recipeconnector_get_api_v1_userrecipe_list", async payload => {
-  const response = await apiService.recipeconnector_get_api_v1_userrecipe_list(payload);
-  return response.data;
-});
-const initialState = {
-  entities: [],
-  api: {
-    loading: "idle",
-    error: null
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const recipeconnector_get_api_v1_userrecipe_list = createAsyncThunk(
+  "recipeconnector_response_get_Listrecipes/recipeconnector_get_api_v1_userrecipe_list",
+  async payload => {
+    const response = await apiService.recipeconnector_get_api_v1_userrecipe_list(
+      payload
+    )
+    return response.data
   }
-};
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
 const recipeconnector_response_get_ListrecipesSlice = createSlice({
   name: "recipeconnector_response_get_Listrecipes",
   initialState,
@@ -18,24 +17,24 @@ const recipeconnector_response_get_ListrecipesSlice = createSlice({
   extraReducers: {
     [recipeconnector_get_api_v1_userrecipe_list.pending]: (state, action) => {
       if (state.api.loading === "idle") {
-        state.api.loading = "pending";
+        state.api.loading = "pending"
       }
     },
     [recipeconnector_get_api_v1_userrecipe_list.fulfilled]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.entities = action.payload;
-        state.api.loading = "idle";
+        state.entities = action.payload
+        state.api.loading = "idle"
       }
     },
     [recipeconnector_get_api_v1_userrecipe_list.rejected]: (state, action) => {
       if (state.api.loading === "pending") {
-        state.api.error = action.error;
-        state.api.loading = "idle";
+        state.api.error = action.error
+        state.api.loading = "idle"
       }
     }
   }
-});
+})
 export default {
   recipeconnector_get_api_v1_userrecipe_list,
   slice: recipeconnector_response_get_ListrecipesSlice
-};
+}
